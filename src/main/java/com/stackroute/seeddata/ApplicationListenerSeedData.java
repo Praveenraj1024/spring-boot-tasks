382a8d23371a62815b78e1bfebbbb292a3c10e28
praveen <praveenrajjoth@gmail.com>
2019-08-05 00:50:14 +0530

    @Value, @PropertySource, Environment, @ConfigurationProperties are used to reduce the hardcoded values.

--- src/main/java/com/stackroute/seeddata/ApplicationListenerSeedData.java
+++ src/main/java/com/stackroute/seeddata/ApplicationListenerSeedData.java
@@ -5,9 +5,12 @@ import com.stackroute.exception.TrackAlreadyExistsException;
 import com.stackroute.service.TrackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,6 +18,8 @@ import org.springframework.stereotype.Component;
  * track details in h2 db.
  */
 @Component
+@PropertySource("classpath:application.properties")
+//Used to specify the path of the resource file.
 public class ApplicationListenerSeedData implements ApplicationListener<ContextRefreshedEvent> {
 
     @Qualifier("trackService")
@@ -26,15 +31,24 @@ public class ApplicationListenerSeedData implements ApplicationListener<ContextR
         this.trackService = trackService;
     }
 
+    @Value("${track1.id}")
+    private int trackId;
+    @Value("${track1.name}")
+    private String trackName;
+    @Value("${track1.comments}")
+    private String trackComments;
+
+    @Autowired
+    private Environment environment;
+
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent){
-        Track track1 = new Track(11, "Penne Penne", "A nice song");
-        Track track2 = new Track(12, "Kadhaley Kadhaley", "melody song");
-        Track track3 = new Track(13, "Karu", "opening song");
+        Track track1 = new Track(trackId, trackName, trackComments);
+        Track track2 = new Track(Integer.parseInt(environment.getProperty("track2.id")),
+                                environment.getProperty("track2.name"), environment.getProperty("track2.comments"));
         try{
             trackService.saveTrack(track1);
             trackService.saveTrack(track2);
-            trackService.saveTrack(track3);
         }catch (TrackAlreadyExistsException ex){
             ex.printStackTrace();
         }
--- src/main/java/com/stackroute/seeddata/CommandLineRunnerSeedData.java
+++ src/main/java/com/stackroute/seeddata/CommandLineRunnerSeedData.java
@@ -2,15 +2,24 @@ package com.stackroute.seeddata;
 
 import com.stackroute.domain.Track;
 import com.stackroute.repository.TrackRepository;
+import lombok.Data;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 /**
  * This class overrides the run() method to store the track details in h2 db.
  */
 @Component
+@Data
+//To create getters and setters for all properties, to override toString(),
+// to create equals, canEquals and HashCode.
+@ConfigurationProperties(prefix = "track4")
+//Used to get the data prefix with track4 and matches with the property name.
 public class CommandLineRunnerSeedData implements CommandLineRunner {
     @Qualifier("trackService")
     //Used to particularly mention the bean name.
@@ -21,13 +30,19 @@ public class CommandLineRunnerSeedData implements CommandLineRunner {
         this.trackRepository = trackRepository;
     }
 
+    private int id;
+    private String name;
+    private String comments;
+
+    @Autowired
+    private Environment environment;
+
     @Override
     public void run(String... args) throws Exception {
-        Track track1 = new Track(14, "Penne Penne", "A nice song");
-        Track track2 = new Track(15, "Kadhaley Kadhaley", "melody song");
-        Track track3 = new Track(16, "Karu", "opening song");
+        Track track1 = new Track(Integer.parseInt(environment.getProperty("track3.id")),
+                                environment.getProperty("track3.name"), environment.getProperty("track3.comments"));
+        Track track2 = new Track(id, name, comments);
         trackRepository.save(track1);
         trackRepository.save(track2);
-        trackRepository.save(track3);
     }
 }
